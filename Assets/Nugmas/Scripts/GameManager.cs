3f64e997eea4dfea07bc75fb629465ee1b10a602
TimeTravelerTom <thomashare@hotmail.com>
2025-12-18 22:27:49 -0500

    Working snowflake grabbing and score tracking, I think.

---
+++ Assets/Nugmas/Scripts/GameManager.cs
@@ -0,0 +1,191 @@
+using UnityEngine;
+
+/// <summary>
+/// Class which manages the gameplay
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    // The script that manages all others
+    public static GameManager instance = null;
+
+    [Tooltip("The player gameobject")]
+    public GameObject player = null;
+
+    [Header("Scores")]
+    // The current player score in the game
+    [Tooltip("The player's score")]
+    [SerializeField] private int gameManagerScore = 0;
+
+    // Static getter/setter for player score (for convenience)
+    public static int score
+    {
+        get
+        {
+            return instance.gameManagerScore;
+        }
+        set
+        {
+            instance.gameManagerScore = value;
+        }
+    }
+
+    // The highest score obtained by this player
+    [Tooltip("The highest score acheived on this device")]
+    public int highScore = 0;
+
+
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called when the script is loaded, called before start
+    /// 
+    /// When this component is first added or activated, setup the global reference
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(this);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HandleStartUp();
+    }
+
+    /// <summary>
+    /// Description:
+    /// Handles necessary activities on start up such as getting the highscore and score, updating UI elements, 
+    /// and checking the number of enemies
+    /// Inputs:
+    /// none
+    /// Returns:
+    /// void (no return)
+    /// </summary>
+    void HandleStartUp()
+    {
+        if (PlayerPrefs.HasKey("highscore"))
+        {
+            highScore = PlayerPrefs.GetInt("highscore");
+        }
+        if (PlayerPrefs.HasKey("score"))
+        {
+            score = PlayerPrefs.GetInt("score");
+        }
+        UpdateUIElements();
+
+    }
+     /// <summary>
+    /// Description:
+    /// Standard Unity function that gets called when the application (or playmode) ends
+    /// Input:
+    /// none
+    /// Return:
+    /// void (no return)
+    /// </summary>
+    private void OnApplicationQuit()
+    {
+        SaveHighScore();
+        ResetScore();
+    }
+
+    /// <summary>
+    /// Description:
+    /// Adds a number to the player's score stored in the gameManager
+    /// Input: 
+    /// int scoreAmount
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    /// <param name="scoreAmount">The amount to add to the score</param>
+    public static void AddScore(int scoreAmount)
+    {
+        score += scoreAmount;
+        if (score > instance.highScore)
+        {
+            SaveHighScore();
+        }
+        UpdateUIElements();
+    }
+    
+    /// <summary>
+    /// Description:
+    /// Resets the current player score
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    public static void ResetScore()
+    {
+        PlayerPrefs.SetInt("score", 0);
+        score = 0;
+    }
+
+    /// <summary>
+    /// Description:
+    /// Saves the player's highscore
+    /// Input: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    public static void SaveHighScore()
+    {
+        if (score > instance.highScore)
+        {
+            PlayerPrefs.SetInt("highscore", score);
+            instance.highScore = score;
+        }
+        UpdateUIElements();
+    }
+
+    /// <summary>
+    /// Description:
+    /// Resets the high score in player preferences
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    public static void ResetHighScore()
+    {
+        PlayerPrefs.SetInt("highscore", 0);
+        if (instance != null)
+        {
+            instance.highScore = 0;
+        }
+        UpdateUIElements();
+    }
+
+    /// <summary>
+    /// Description:
+    /// Sends out a message to UI elements to update
+    /// Input: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    public static void UpdateUIElements()
+    {
+        if (OutsideSceneUI.instance != null)
+        {
+            OutsideSceneUI.instance.UpdateScore();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
