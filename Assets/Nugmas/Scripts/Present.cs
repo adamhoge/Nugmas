93cdeebaf78811a9f6df11b941a7fc85855cfbf3
Adam <adhoge@gmail.com>
2025-12-22 22:28:51 -0500

    Add more outfits

--- Assets/Nugmas/Scripts/Present.cs
+++ Assets/Nugmas/Scripts/Present.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 
 public class Present : MonoBehaviour
 {
-
     public GameObject GetEffect = null;
     public GameObject DropEffect = null;
 
     public GameObject ShowEffect = null;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
-     private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Present Trigger");
         Debug.Log(collision.tag);
         if (collision.tag == "Ground")
         {
             Debug.Log("crash");
-            if(GameManager.instance) { GameManager.ResetScore(); }
-            if(OutsideSceneUI.instance) { OutsideSceneUI.instance.UpdateScore(); }    
+            if (GameManager.instance)
+            {
+                GameManager.ResetScore();
+            }
+            if (OutsideSceneUI.instance)
+            {
+                OutsideSceneUI.instance.UpdateScore();
+            }
 
             GameObject sfx = Instantiate(DropEffect, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
@@ -35,30 +35,38 @@ public class Present : MonoBehaviour
         else if (collision.gameObject.tag == "Player")
         {
             Debug.Log("unwrap!");
-            if(GameManager.instance) { GameManager.AddScore(1); }
-            if(OutsideSceneUI.instance) { OutsideSceneUI.instance.UpdateScore(); }    
-            if(SantaClaus.instance) { 
+            if (GameManager.instance)
+            {
+                GameManager.AddScore(1);
+            }
+            if (OutsideSceneUI.instance)
+            {
+                OutsideSceneUI.instance.UpdateScore();
+            }
+            if (SantaClaus.instance)
+            {
                 Debug.Log("Calling Santa.");
-                SantaClaus.instance.SetNextPresentTime(System.DateTime.Now, SantaClaus.instance.waitTime); 
-                }
+                SantaClaus.instance.SetNextPresentTime(
+                    System.DateTime.Now,
+                    SantaClaus.instance.waitTime
+                );
+            }
 
             ItemData gottenItem = StuffManager.AddRandomItem();
 
-            
-            if(!(gottenItem == null)) 
+            if (!(gottenItem == null))
             {
                 ParticleSystem presentPS = ShowEffect.GetComponent<ParticleSystem>();
                 presentPS.textureSheetAnimation.SetSprite(0, gottenItem.Sprite);
-                Instantiate(ShowEffect, transform.position, Quaternion.identity); 
+                Instantiate(ShowEffect, transform.position, Quaternion.identity);
             }
-        
+
             GameObject sfx = Instantiate(GetEffect, transform.position, Quaternion.identity);
             AudioSource ding = sfx.GetComponent<AudioSource>();
-            float adj = Mathf.Round(Random.Range(0f,0.3f) * 100) / 100;  
+            float adj = Mathf.Round(Random.Range(0f, 0.3f) * 100) / 100;
             ding.pitch = 1 + adj;
             Debug.Log(ding.pitch);
             Destroy(this.gameObject);
         }
     }
-
 }
